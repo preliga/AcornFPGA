18069b6e6fe04b3abf60c3c5f76f7448850302b0
Hubert Rytel <h.rytel@gmail.com>
2017-06-22 22:19:50 +0200

    zmienione pliki

--- ACORN/FBK128.vhd
+++ ACORN/FBK128.vhd
@@ -7,8 +7,8 @@ ENTITY FBK128 IS PORT
 (
 	S		:IN STD_LOGIC_VECTOR(292 downto 0);
 	KS		:IN STD_LOGIC_VECTOR(31 downto 0);
-	CA		:IN STD_LOGIC;
-	CB		:IN STD_LOGIC;
+	CA		:IN STD_LOGIC_VECTOR(31 downto 0);
+	CB		:IN STD_LOGIC_VECTOR(31 downto 0);
 	
 	OPT	:OUT STD_LOGIC_VECTOR(31 downto 0)
 );
@@ -16,4 +16,9 @@ END ENTITY;
 
 ARCHITECTURE A1 OF FBK128 IS
 BEGIN
+
+OPT <= S(31 downto 0) xor (not S(138 downto 107)) xor
+		(S(275 downto 244) and S(54 downto 23)) xor (S(275 downto 244) and S(191 downto 160)) xor (S(54 downto 23) and S(191 downto 160)) xor
+		(S(227 downto 196) and CA) xor (KS and CB);
+
 END ARCHITECTURE;
